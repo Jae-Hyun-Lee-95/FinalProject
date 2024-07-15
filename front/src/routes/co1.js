1ee31bcbd133c0bb0623708d680cb307a2c61cf0
Lee-Jae-Hyun-95 <dlwogus588866@naver.com>
2024-07-15 15:24:16 +0900

    asd

--- front/src/routes/co1.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const co1 = () => {
-  return <h1>co1 페이지 내용</h1>;
-};
-export default co1;
--- front/src/routes/login.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const login = () => {
-  return <h1>login 페이지 내용</h1>;
-};
-export default login;
