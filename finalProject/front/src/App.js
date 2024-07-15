1d778f115ff46d5c1e5b252415bbe1c0b9a9be8d
Lee-Jae-Hyun-95 <dlwogus588866@naver.com>
2024-07-15 10:42:53 +0900

    2024-07-15 시작

---
+++ finalProject/front/src/App.js
@@ -0,0 +1,48 @@
+// src/App.js
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './views/header/components/Header';
+
+import Ju1 from './routes/ju1';
+import Ju2 from './routes/ju2';
+import Co1 from './routes/co1';
+import Co2 from './routes/co2';
+import Budongsan from './views/budongsan/Budongsan';
+import Bu2 from './routes/bu2';
+import Back1 from './routes/back1';
+import Chat1 from './routes/chat1';
+import Comu1 from './routes/comu1';
+import Login from './routes/login';
+
+
+import{
+  BrowserRouter, Routes, Route
+} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path='/ju1' element={<Ju1/>}/>
+          <Route path='/ju2' element={<Ju2/>}/>
+          <Route path='/co1' element={<Co1/>}/>
+          <Route path='/co2' element={<Co2/>}/>
+          <Route path='/budongsan' element={<Budongsan/>}/>
+          <Route path='/bu2' element={<Bu2/>}/>
+          <Route path='/back1' element={<Back1/>}/>
+          <Route path='/chat1' element={<Chat1/>}/>
+          <Route path='/comu1' element={<Comu1/>}/>
+          <Route path='/login' element={<Login/>}/>
+
+        
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ finalProject/front/src/routes/chat1.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const chat1 = () => {
+  return <h1>chat1 페이지 내용</h1>;
+};
+export default chat1;
