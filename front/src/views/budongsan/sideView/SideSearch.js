66d88b7c4eb17acd6aadf97e5a4cd0dbe6f41874
Lee-Jae-Hyun-95 <dlwogus588866@naver.com>
2024-07-15 11:07:46 +0900

    123

--- front/src/views/budongsan/sideView/SideSearch.js
+++ front/src/views/budongsan/sideView/SideSearch.js
@@ -1,12 +1,33 @@
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const SideSearch = () => {
+
+    const [members, setMembers] = useState([]);
+
+    useEffect(() => {
+        // Flask 서버로부터 데이터를 받아옴
+        axios.get('http://localhost:5000/users')
+            .then(response => {
+                setMembers(response.data.members);
+            })
+            .catch(error => {
+                console.error("There was an error fetching the data!", error);
+            });
+    }, []);
+    
     return(
         <div>
             <form class="d-flex">
                 <input class="form-control me-sm-2" type="search" placeholder="Search"/>
                 <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                
             </form>
+            <ul>
+                {members.map(member => (
+                    <li key={member.id}>{member.name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
